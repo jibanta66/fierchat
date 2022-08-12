a066e1b9788478ee9ebe0246a41befe0da62fbe2
Jibanta <63874888+jibanta66@users.noreply.github.com>
2022-08-12 23:30:12 +0700

    2

--- src/App.js
+++ src/App.js
@@ -10,7 +10,13 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 firebase.initializeApp({
-  // your config
+  apiKey: "AIzaSyBAM0lqNN02UpmIehJXXWDFOEoCyW7UpTQ",
+  authDomain: "fierbasechat-b1b5d.firebaseapp.com",
+  projectId: "fierbasechat-b1b5d",
+  storageBucket: "fierbasechat-b1b5d.appspot.com",
+  messagingSenderId: "256763580850",
+  appId: "1:256763580850:web:90d2cb3fdac3e7900e14ab",
+  measurementId: "G-GN4ZT0QKGX"
 })
 
 const auth = firebase.auth();
